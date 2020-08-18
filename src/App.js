785444759f97de5e5063f9b944d47d4176b45d16
Carlos Moreno <soyelmoreno@yahoo.com>
2020-08-18 16:06:16 -0700

    useState, custome hooks, update ESLint, bring in petfinder client

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
-// We create a component which is just a function that returns the result
-// of React.createElement().
 const App = () => {
-  // return React.createElement(
-  //   "div",
-  //   {
-  //     id: "something-important",
-  //   },
-  //   [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Alex",
-  //       animal: "Dog",
-  //       breed: "Golden Lab mix",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Puddin",
-  //       animal: "Cat",
-  //       breed: "Tabby",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Zoe",
-  //       animal: "Dog",
-  //       breed: "Weiner dog",
-  //     })
-  //   ]
-  // );
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Alex" animal="Dog" breed="Golden lab mix" />
-      <Pet name="Puddin" animal="Cat" breed="Tabby" />
-      <Pet name="Zoe" animal="Dog" breed="Weiner dog" />
+      <SearchParams />
     </div>
   );
 };
-// A component is just something that returns markup. A rubber stamp.
-// When you call it it creates an element, and you can stamp out as many
-// as you want.
 
-// So now let's render this to the DOM
-// createElement:
-// first: what kind of tag is it. Either takes a composite component that you
-// wrote or a string with the tag name of the element you want to create.
-// second: all the attributes you want to give the component, the child element. Can omit.
-// third: the children. Can be single or an array. Can omit.
-
-// When you say .render() and give it an element, it blows away existing contents,
-// replaces it with what you are creating.
 render(<App />, document.getElementById("root"));
