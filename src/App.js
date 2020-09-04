e52f364d9049ea56a32addae6d0cf640b5ae1fe7
Carlos Moreno <soyelmoreno@yahoo.com>
2020-09-03 23:03:14 -0700

    Add Reach Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import React from "react";
 // Imports only the render method exported from the reactDOM package
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+// Import Reach Router and also the second page
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          {/* Reach Router uses a scoring system to choose the route that matches *the best*.
+          So, order does not matter (very much)*/}
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
