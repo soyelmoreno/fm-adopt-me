a86419a50c7d873fa7c2102413a9ffe74eed22a9
Carlos Moreno <soyelmoreno@yahoo.com>
2020-10-19 12:34:18 -0700

    Add index.js file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// Imports only the render method exported from the reactDOM package
+import {render} from 'react-dom';
+import App from './App';
+
+render(<App />, document.getElementById('root'));
